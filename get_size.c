e69414fe070674bd506c95fc93440c6607374b32
DonaldoAkech <chrisdona95@gmail.com>
2023-08-21 18:03:47 +0300

    get_size.c

--- get_size.c
+++ get_size.c
@@ -6,14 +6,14 @@
  * @format: Formatted string in which to print the arguments
  * @i: Arguments lists to be printed.
  *
- * Return: precision.
+ * Return: size.
  */
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
 	int size = 0;
 
-	if (format[a] == 'l')
+	if (format[curr_i] == 'l')
 		size = S_LONG;
 	else if (format[curr_i] == 'h')
 		size = S_SHORT;
